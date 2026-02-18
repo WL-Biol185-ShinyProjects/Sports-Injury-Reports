11d396c3645aacf32a545aadbe4438c8a7c7053c
Cora Villere <cvillere@mail.wlu.edu>
2026-02-18 13:10:48 -0500

    Committing before I pull

--- ui.R
+++ ui.R
@@ -1,1 +1,19 @@
 library(shiny)
+
+
+fluidPage(
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "year", 
+                  label = "Year",
+                  min = 2007,
+                  max = 2024,
+                  value = 2007
+                  )
+    ),
+    mainPanel(
+      plotOutput("Yearly Injuries By Sport")
+    )
+  )
+)
