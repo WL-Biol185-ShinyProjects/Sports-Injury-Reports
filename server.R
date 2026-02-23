4715d55e16de0f5d35959eae47ea44878bef3581
Serenna Wu <swu1@mail.wlu.edu>
2026-02-23 16:07:26 -0500

    fixed some errors and add more element to the graph

--- server.R
+++ server.R
@@ -1,18 +1,54 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
+library(plotly)
+
 yearly_injuries_final <- read.csv("yearly_injuries_final.csv") %>%
-mutate(injuries = as.numeric(gsub(",", "", injuries)))
+  
+#Fixing the Table
+  mutate(injuries = as.numeric(gsub(",", "", injuries)),
+         year = as.numeric(year),
+         sport_or_activity = trimws(sport_or_activity)) %>%
+  group_by(sport_or_activity, year) %>%
+  summarise(injuries = sum(injuries, na.rm = TRUE), .groups = "drop")
 
 
 function(input, output) {
-  output$yearly_injuries_by_sport <- renderPlot({
-    yearly_injuries_final %>%
+#yearly injuries by year
+  output$yearly_injuries_by_sport <- renderPlotly({
+    
+    data <- yearly_injuries_final %>%
       arrange(injuries) %>%
-      filter(year == input$year) %>%
-      mutate(sport_or_activity = factor(sport_or_activity, levels = sport_or_activity, ordered = TRUE)) %>%
-      ggplot(aes(x = sport_or_activity, y = injuries)) + geom_col() + 
-      scale_y_continuous(labels = scales::comma) + 
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+      filter(year == input$year)
+    
+    if(nrow(data) == 0) {
+      p <- ggplot() + 
+        annotate("text", x = 0.5, y = 0.5, 
+                 label = "No data available for this year", 
+                 size = 6) +
+        theme_void()
+      return(ggplotly(p))
+    }
+    
+    p <- data %>%
+      mutate(sport_or_activity = factor(sport_or_activity, 
+                                        levels = sport_or_activity, 
+                                        ordered = TRUE)) %>%
+      ggplot(aes(x = sport_or_activity, 
+                 y = injuries,
+                 text = paste("Sport:", sport_or_activity,
+                              "<br>Injuries:", scales::comma(injuries)))) + 
+      geom_col() +
+      scale_y_continuous(labels = scales::comma,
+                         expand = expansion(mult = c(0,0.05))) + 
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 7),
+            plot.margin = margin(b = 100, r = 20, t = 20, l = 20, unit = "pt")
+            )
+    
+    ggplotly(p, tooltip = "text") %>%
+      layout(height = 1000)
   })
+  
+#injuries by age
+  
 }
--- ui.R
+++ ui.R
@@ -1,20 +1,25 @@
 library(shiny)
+library(plotly)
+
 
 
 fluidPage(
-  
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "year", 
-                  label = "year",
-                  min = 2007,
-                  max = 2024,
-                  value = 2007
-                  )
+  tabsetPanel(
+    tabPanel("Yearly Injuries by Sport",
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput(inputId = "year",
+                             label = "year",
+                             min = 2007,
+                             max = 2024,
+                             value = 2007,
+                             sep = "")
+               ),
+               mainPanel(
+                 plotlyOutput("yearly_injuries_by_sport")
+               )
+             )
     ),
-    mainPanel(
-      plotOutput("yearly_injuries_by_sport")
-    )
+    tabPanel("Injuries by Age")
   )
 )
-
